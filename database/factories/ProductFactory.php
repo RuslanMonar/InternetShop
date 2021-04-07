1531dc68d8ef0b3a6645a0b3f6787f7370fce04e
Ruslan_monar <ruslanmonar@gmail.com>
2021-04-07 18:17:52 +0300

    add comments to table + AppServeceProvider aliace

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -22,11 +22,11 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            'product_name' => 'Apple iPhone 12 Pro Max 256GB Pacific Blue (MGDF3)',
-            'price' => 44999,
-            'quantity' => 120,
-            'rating' => 4.9,
-            'front_image' => 'https://i.allo.ua/media/catalog/product/cache/1/image/710x600/602f0fa2c1f0d1ba5e241f914e856ff9/i/p/iphone-12-pro-max-blue-hero_4.jpg'
+            'product_name' => 'Xiaomi Mi 11 8/256GB Horizon Blue',
+            'price' => 26999,
+            'quantity' => 5,
+            'rating' => 4.6,
+            'front_image' => 'https://i.allo.ua/media/catalog/product/cache/1/image/710x600/602f0fa2c1f0d1ba5e241f914e856ff9/x/i/xiaomi_mi_11_horizon_blue_1_1.jpg'
         ];
     }
 }
@@ -40,3 +40,13 @@ class ProductFactory extends Factory
 //     'rating' => 4.6,
 //     'front_image' => 'https://i.allo.ua/media/catalog/product/cache/1/image/710x600/602f0fa2c1f0d1ba5e241f914e856ff9/x/i/xiaomi_mi_11_horizon_blue_1_1.jpg'
 // ];
+
+
+//-------------------------------
+// return [
+//             'product_name' => 'Apple iPhone 12 Pro Max 256GB Pacific Blue (MGDF3)',
+//             'price' => 44999,
+//             'quantity' => 120,
+//             'rating' => 4.5,
+//             'front_image' => 'https://i.allo.ua/media/catalog/product/cache/1/image/710x600/602f0fa2c1f0d1ba5e241f914e856ff9/i/p/iphone-12-pro-max-blue-hero_4.jpg'
+//         ];
