dbc019e3e7b121fa4c93af75d7a48be25b9666f5
Ruslan_monar <ruslanmonar@gmail.com>
2021-04-09 20:06:17 +0300

    created tables + add few products to db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
         Relation::morphMap([
             'Phone' => \App\Models\Phone::class,
             'Laptop' => \App\Models\Laptop::class,
+            'Teblet' => \App\Models\Tablet::class,
         ]);
     }
 }
