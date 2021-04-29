1f36088e132aaae377cb8b4b4d6f4394106a5efb
Ruslan_monar <ruslanmonar@gmail.com>
2021-04-30 00:13:39 +0300

    fill products db

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\UserController;
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
