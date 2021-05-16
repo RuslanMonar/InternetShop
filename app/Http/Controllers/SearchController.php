3fb6717c2f5ab10eba55b40dc0fe2540df89bcf4
Ruslan_monar <ruslanmonar@gmail.com>
2021-05-17 01:16:26 +0300

    pagination

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,7 +16,16 @@ class SearchController extends Controller
     {
         $search = $request->get("name");
         // $result = Product::all()->where("product_name",$request->get("name"));
-        $result = Product::where(DB::raw('product_name'), 'like', '%'.$search.'%')->get();
+        $result = Product::where(DB::raw('product_name'), 'like', '%'.$search.'%')->paginate(12);
+        $result = $this->EditImgPath($result);
         return $result ?  response()->json(['Answer' => 'Ok','search' => $result],200):  response()->json(['Answer' => 'Failed'],401);
     }
+    public function EditImgPath($products)
+    {
+        foreach ($products as $product) {
+            $product->front_image = 'http://localhost:8000' . $product->front_image;
+        }
+        return $products;
+    }
+
 }
