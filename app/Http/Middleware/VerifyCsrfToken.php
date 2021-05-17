46e8728819f153aa200c77edef92a97c3cfdc309
Ruslan_monar <ruslanmonar@gmail.com>
2021-05-18 02:33:00 +0300

    product details load

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,6 +15,7 @@ class VerifyCsrfToken extends Middleware
         '/api/category',
         '/api/filter',
         '/api/products',
-        '/api/search'
+        '/api/search',
+        'api/product-details'
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,4 @@ Route::middleware('auth:sanctum')->post('/change-quantity-value',[CartController
 
 Route::middleware('auth:sanctum')->post('/count-products-in-cart',[CartController::class,'CountProductInCart']);
 
+Route::post('/product-details',[ProductsController::class,'ProductDetails']);
